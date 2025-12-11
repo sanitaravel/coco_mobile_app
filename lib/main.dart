b626dc33dee5a3c93d79f4828e8e52e5b05abcb7
sanitaravel <alexander.koshcheev.msk@gmail.com>
2025-12-11 22:30:33 +0100

    feat: refactor MainApp to use StatefulWidget and implement page navigation

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,15 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  MainAppState createState() => MainAppState();
+}
+
+class MainAppState extends State<MainApp> {
+  final PageController _pageController = PageController();
 
   static const List<Widget> _pages = <Widget>[
     Dashboard(),
@@ -40,6 +47,13 @@ class MainApp extends StatelessWidget {
         ),
         home: BlocBuilder<NavigationBloc, NavigationState>(
           builder: (context, state) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              _pageController.animateToPage(
+                state.currentIndex,
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.ease,
+              );
+            });
             return Scaffold(
               appBar: AppBar(
                 title: Text(
@@ -62,7 +76,10 @@ class MainApp extends StatelessWidget {
                 backgroundColor: Colors.transparent,
                 elevation: 0,
               ),
-              body: _pages[state.currentIndex],
+              body: PageView(
+                controller: _pageController,
+                children: _pages,
+              ),
               bottomNavigationBar: Container(
                 height: 70,
                 padding: EdgeInsets.symmetric(horizontal: 30),
